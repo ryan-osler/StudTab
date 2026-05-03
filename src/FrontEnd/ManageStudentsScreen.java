22b76c06d1ea182183ba68d6101198823d1a6a98
ryano <ryano@DESKTOP-LG0QQE0>
2026-05-03 14:12:08 +0200

    This Version should work

--- src/FrontEnd/ManageStudentsScreen.java
+++ src/FrontEnd/ManageStudentsScreen.java
@@ -8,6 +8,7 @@ import BackEnd.Teacher;
 import BackEnd.TimetableManager;
 import BackEnd.UserManager;
 import java.awt.Color;
+import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -22,6 +23,10 @@ public class ManageStudentsScreen extends javax.swing.JFrame {
     private MainMenuScreen mainMenu;
     public ManageStudentsScreen(MainMenuScreen mm) {
         initComponents();
+        btnSetup();
+        boolean[] canEdit = new boolean [] {
+            false, false, false, false, false, false, true  // ← last one
+        };
         this.mainMenu = mm;
         setTblStudents();
         loadTblStudents();
@@ -29,8 +34,44 @@ public class ManageStudentsScreen extends javax.swing.JFrame {
     }
     public ManageStudentsScreen(){
         initComponents();
+        btnSetup();
+        boolean[] canEdit = new boolean [] {
+            false, false, false, false, false, false, true  // ← last one
+        };
         setTblStudents();
         loadTblStudents();
+    }
+    
+    private void btnSetup(){//hovver effect for buttons. this shiz took so long
+        Color norm = new Color(47,56,120);
+        btnSave.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent evt){
+                btnSave.setForeground(Color.WHITE);
+                btnSave.setBorder(new LineBorder(Color.WHITE));
+            }
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent evt){
+                btnSave.setForeground(norm);
+                btnSave.setBorder(new LineBorder(norm));
+            }
+            @Override
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                btnSave.setForeground(norm);
+                btnSave.setBorder(new LineBorder(norm));
+            }
+            @Override
+            public void mouseReleased(java.awt.event.MouseEvent evt){
+                if (btnSave.contains(evt.getPoint())) {
+                    btnSave.setForeground(Color.WHITE);
+                    btnSave.setBorder(new LineBorder(Color.WHITE));
+                }else{
+                    btnSave.setForeground(norm);
+                    btnSave.setBorder(new LineBorder(norm));
+                }
+                
+            }
+        });
     }
     private void loadTblStudents(){
         DefaultTableModel model = (DefaultTableModel) tblStudents.getModel();
@@ -125,9 +166,73 @@ public class ManageStudentsScreen extends javax.swing.JFrame {
                 }
             }
         );
-        tblStudents.setDefaultEditor(Object.class, null);
+        tblStudents.getColumnModel().getColumn(6).setCellRenderer(new EditRenderer());
+        tblStudents.getColumnModel().getColumn(6).setCellEditor(new EditEditor());
     }
     
+    private class EditRenderer extends javax.swing.table.DefaultTableCellRenderer{
+        private final javax.swing.JButton btn = new javax.swing.JButton("Edit");
+        
+        EditRenderer(){
+            btn.setBackground(new Color(47,56,120));
+            btn.setForeground(Color.WHITE);
+            btn.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 13));
+            btn.setBorderPainted(false);
+            btn.setOpaque(true);
+        }
+        
+        @Override
+        public java.awt.Component getTableCellRendererComponent(
+                javax.swing.JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column){
+            return btn;
+        }
+    }
+    
+    private class EditEditor extends javax.swing.DefaultCellEditor{
+        private final javax.swing.JButton btn;
+        
+        EditEditor(){
+            super(new javax.swing.JCheckBox());
+            btn = new javax.swing.JButton("Edit");
+            btn.setBackground(new Color(47,56,120));
+            btn.setForeground(Color.WHITE);
+            btn.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 13));
+            btn.setBorderPainted(false);
+            btn.setOpaque(true);
+            
+            btn.addActionListener(e -> {
+                int row = tblStudents.getSelectedRow();
+                if (row >=0) {
+                    String name = (String) tblStudents.getValueAt(row, 0);
+                    String surname = (String) tblStudents.getValueAt(row, 1);
+                    String email = (String) tblStudents.getValueAt(row, 2);
+                    String password = (String) tblStudents.getValueAt(row, 3);
+                    String DOB = (String) tblStudents.getValueAt(row, 4);
+                    String grade = String.valueOf(tblStudents.getValueAt(row, 5));
+                    
+                    //TODO
+                    txfName.setText(name);
+                    txfSurname.setText(surname);
+                    txfEmail.setText(email);
+                    txfPassword.setText(password);
+                    txfDOB.setText(DOB);
+                    txfGrade.setText(grade);
+                    System.out.println("Edit Clicked: "+name+" "+surname);
+                }
+                fireEditingStopped();
+            });
+        }
+        
+        @Override
+        public java.awt.Component getTableCellEditorComponent(
+            javax.swing.JTable table, Object value, boolean isSelected, int row, int column){
+            return btn;
+        }
+        
+        @Override public Object getCellEditorValue(){ 
+            return "Edit";
+        }
+    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -145,6 +250,18 @@ public class ManageStudentsScreen extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         tblStudents = new javax.swing.JTable();
         jLabel2 = new javax.swing.JLabel();
+        txfName = new javax.swing.JTextField();
+        txfSurname = new javax.swing.JTextField();
+        txfEmail = new javax.swing.JTextField();
+        txfDOB = new javax.swing.JTextField();
+        txfPassword = new javax.swing.JTextField();
+        txfGrade = new javax.swing.JTextField();
+        lblName = new javax.swing.JLabel();
+        lblSurname = new javax.swing.JLabel();
+        lblPassword = new javax.swing.JLabel();
+        lblDOB = new javax.swing.JLabel();
+        lblGrade = new javax.swing.JLabel();
+        btnSave = new javax.swing.JButton();
         lblBackground = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -183,7 +300,7 @@ public class ManageStudentsScreen extends javax.swing.JFrame {
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, false, false, false, false, false
+                false, false, false, false, false, false, true
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
@@ -215,6 +332,36 @@ public class ManageStudentsScreen extends javax.swing.JFrame {
         jLabel2.setForeground(new java.awt.Color(47, 56, 120));
         jLabel2.setText("Edit Student:");
         jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 420, -1, -1));
+        jPanel1.add(txfName, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 450, 120, -1));
+        jPanel1.add(txfSurname, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 480, 120, -1));
+
+        txfEmail.setEditable(false);
+        jPanel1.add(txfEmail, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 420, 210, -1));
+        jPanel1.add(txfDOB, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 480, 90, -1));
+        jPanel1.add(txfPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 450, 130, -1));
+        jPanel1.add(txfGrade, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 450, 50, -1));
+
+        lblName.setText("Name:");
+        jPanel1.add(lblName, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 450, -1, -1));
+
+        lblSurname.setText("Surname:");
+        jPanel1.add(lblSurname, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 480, -1, -1));
+
+        lblPassword.setText("Password:");
+        jPanel1.add(lblPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 450, -1, -1));
+
+        lblDOB.setText("Date of Birth:");
+        jPanel1.add(lblDOB, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 480, -1, -1));
+
+        lblGrade.setText("Grade:");
+        jPanel1.add(lblGrade, new org.netbeans.lib.awtextra.AbsoluteConstraints(660, 450, -1, -1));
+
+        btnSave.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 0, 18)); // NOI18N
+        btnSave.setForeground(new java.awt.Color(47, 56, 120));
+        btnSave.setText("Save");
+        btnSave.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(47, 56, 120)));
+        btnSave.setContentAreaFilled(false);
+        jPanel1.add(btnSave, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 530, 70, -1));
 
         lblBackground.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/loginBackground.png"))); // NOI18N
         lblBackground.setText("jLabel1");
@@ -273,13 +420,25 @@ public class ManageStudentsScreen extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnSave;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblBackground;
+    private javax.swing.JLabel lblDOB;
+    private javax.swing.JLabel lblGrade;
+    private javax.swing.JLabel lblName;
+    private javax.swing.JLabel lblPassword;
+    private javax.swing.JLabel lblSurname;
     private javax.swing.JTable tblStudents;
+    private javax.swing.JTextField txfDOB;
+    private javax.swing.JTextField txfEmail;
+    private javax.swing.JTextField txfGrade;
+    private javax.swing.JTextField txfName;
+    private javax.swing.JTextField txfPassword;
     private javax.swing.JTextField txfSearchStudent;
+    private javax.swing.JTextField txfSurname;
     // End of variables declaration//GEN-END:variables
 }
